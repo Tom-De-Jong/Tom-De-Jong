ab2fbea41441fd6517e447828a7705a491381206
Tom-De-Jong <tomdejong2009@gmail.com>
2025-11-20 21:26:17 +0100

    aa

--- hamburger.js
+++ hamburger.js
@@ -11,3 +11,26 @@ function Hamburger(){
         HamburgerMenu.classList="HamburgerMenuOff"
     }
 }
+
+function CopyEmail(){
+  var copyText = document.getElementById("emailCopy");
+  var change = document.getElementById("changeEmail")
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  navigator.clipboard.writeText(copyText.value);
+  change.innerHTML = "Copied"
+
+}
+
+function CopyInsta(){
+  var copyText = document.getElementById("instagramCopy");
+    var change = document.getElementById("changeInsta")
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  navigator.clipboard.writeText(copyText.value);
+  change.innerHTML = "Copied"
+}
